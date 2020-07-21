581db2cd6d74f50ae698f38509a3966d3e2bc7a3
farhandzakyarvianto <farhandzaky25@gmail.com>
2020-07-21 11:44:21 +0700

    -

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => {
+    const [advice, setAdvice] = useState("");
+
+    useEffect(() => {
+        fetchAdvice();
+    }, []);
+
+    const fetchAdvice = () => {
+        axios
+            .get(`https://api.adviceslip.com/advice`)
+            .then((response) => {
+                const { advice } = response.data.slip;
+                setAdvice(advice);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    return (
+        <div className="app">
+            <div className="app__card">
+                <h1 className="app__heading">{advice}</h1>
+                <button className="app__button" onClick={fetchAdvice}>
+                  <span>GIVE ME ADVICE</span>
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
